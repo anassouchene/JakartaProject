a6363b05b534779d8c7315286e2b3ffa32e23c07
anas <anass_hamzaoui@hotmail.com>
2024-01-18 00:24:55 +0100

    ViewModel Package

---
+++ src/main/java/ViewModel/UserVM.java
@@ -0,0 +1,27 @@
+package ViewModel;
+
+public class UserVM {
+	
+	private String Login;
+	private String Password;
+	
+	
+	public String getLogin() {
+		return Login;
+	}
+	
+	
+	public void setLogin(String login) {
+		Login = login;
+	}
+	
+	
+	public String getPassword() {
+		return Password;
+	}
+	
+	
+	public void setPassword(String password) {
+		this.Password = password;
+	}
+}
